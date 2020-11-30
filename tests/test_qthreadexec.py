f6a4bbb0774460df0f5e8b789b84dcd9d5f46bbb
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2020-11-30 13:59:52 +0100

    Ensure sufficient stack size for worker threads

--- tests/test_qthreadexec.py
+++ tests/test_qthreadexec.py
@@ -35,3 +35,15 @@ def test_ctx_after_shutdown(shutdown_executor):
 def test_submit_after_shutdown(shutdown_executor):
     with pytest.raises(RuntimeError):
         shutdown_executor.submit(None)
+
+
+def test_stack_recursion_limit(executor):
+    # Test that worker threads have sufficient stack size for the default
+    # sys.getrecursionlimit. If not this should fail with SIGSEGV or SIGBUS
+    # (or event SIGILL?)
+    def rec(a, *args, **kwargs):
+        rec(a, *args, **kwargs)
+    fs = [executor.submit(rec, 1) for _ in range(10)]
+    for f in fs:
+        with pytest.raises(RecursionError):
+            f.result()

d79c437d1bd63ade7bdc90bf1f1ffe3d90cb07a7
TheGreatCabbage <20987172+TheGreatCabbage@users.noreply.github.com>
2020-12-10 20:34:11 +0000

    Add Python 3.9 to setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
         "Environment :: X11 Applications :: Qt",
     ],

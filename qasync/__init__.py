ee9b0a3ab9548240c71d4a52e6c6a060a633e930
Alex March <alexmarch@fastmail.com>
2023-10-06 19:37:20 +0900

    Fix release workflow permissions

--- qasync/__init__.py
+++ qasync/__init__.py
@@ -14,9 +14,6 @@ __author__ = (
     "Mark Harviston <mark.harviston@gmail.com>, "
     "Arve Knudsen <arve.knudsen@gmail.com>",
 )
-__version__ = "0.25.0"
-__url__ = "https://github.com/CabbageDevelopment/qasync"
-__license__ = "BSD"
 __all__ = ["QEventLoop", "QThreadExecutor", "asyncSlot", "asyncClose"]
 
 import asyncio

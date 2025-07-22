3ed12df89726da0aecb8c291f8fcb603505eaaa5
John <johnzhou721@gmail.com>
2025-07-21 21:22:32 -0500

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,13 +1,12 @@
 import qasync
 import asyncio
 
-def test_run_with_contextmanager():
+def test_run_with_contextmanager(application):
     async def coro():
         event_loop = asyncio.get_event_loop()
         assert type(event_loop).__name__ == "QSelectorEventLoop"
         await asyncio.sleep(0)
 
-    app = QApplication([])
     qasync.run(coro())
 
     event_loop = asyncio.get_event_loop()

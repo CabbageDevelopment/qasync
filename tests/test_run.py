a1c9df200be192079b600cc2654903365d30f9e6
John <johnzhou721@gmail.com>
2025-07-21 21:18:25 -0500

    Create test_run.py

---
+++ tests/test_run.py
@@ -0,0 +1,13 @@
+import qasync
+import asyncio
+
+def test_run_with_contextmanager():
+    async def coro():
+        event_loop = asyncio.get_event_loop()
+        assert type(event_loop).__name__ == "QSelectorEventLoop"
+        await asyncio.sleep(0)
+
+    qasync.run(coro())
+
+    event_loop = asyncio.get_event_loop()
+    assert type(event_loop).__name__ != "QSelectorEventLoop"

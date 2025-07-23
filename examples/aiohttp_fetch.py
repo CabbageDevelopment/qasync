fccccb8963bf6a0637bfa32c5dd685984c362bdb
Kristján Valur Jónsson <sweskman@gmail.com>
2025-07-23 14:26:31 +0000

    update aiohttp example

--- examples/aiohttp_fetch.py
+++ examples/aiohttp_fetch.py
@@ -68,15 +68,14 @@ class MainWindow(QWidget):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    event_loop = QEventLoop(app)
-    asyncio.set_event_loop(event_loop)
-
     app_close_event = asyncio.Event()
     app.aboutToQuit.connect(app_close_event.set)
     
     main_window = MainWindow()
     main_window.show()
 
-    event_loop.create_task(main_window.boot())
-    event_loop.run_until_complete(app_close_event.wait())
-    event_loop.close()
+    async def async_main():
+        asyncio.create_task(main_window.boot())
+        await app_close_event.wait()
+    
+    asyncio.run(async_main(), loop_factory=lambda:QEventLoop(app))

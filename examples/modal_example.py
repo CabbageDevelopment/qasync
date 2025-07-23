f8bdd5882497ebaf5fc353e1370d30500dd51e6e
Kristján Valur Jónsson <sweskman@gmail.com>
2025-07-23 16:44:44 +0000

    add example for call_sync

---
+++ examples/modal_example.py
@@ -0,0 +1,48 @@
+import asyncio
+import sys
+
+# from PyQt6.QtWidgets import
+from PySide6.QtWidgets import QApplication, QProgressBar, QMessageBox
+from qasync import QEventLoop, call_sync
+
+
+async def master():
+    progress = QProgressBar()
+    progress.setRange(0, 99)
+    progress.show()
+    await first_50(progress)
+    
+
+async def first_50(progress):
+    for i in range(50):
+        progress.setValue(i)
+        await asyncio.sleep(0.1)
+    
+    # Schedule the last 50% to run asynchronously
+    asyncio.create_task(
+        last_50(progress)
+    )
+    
+    # create a notification box, use helper to make entering event loop safe.
+    result = await call_sync(
+        lambda: QMessageBox.information(
+            None, "Task Completed", "The first 50% of the task is completed."
+        )
+    )
+    assert result == QMessageBox.StandardButton.Ok
+
+
+async def last_50(progress):
+    for i in range(50, 100):
+        progress.setValue(i)
+        await asyncio.sleep(0.1)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    event_loop = QEventLoop(app)
+    asyncio.set_event_loop(event_loop)
+
+    event_loop.run_until_complete(master())
+    event_loop.close()

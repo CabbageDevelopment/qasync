4def8cc4e152eadbd8f2aa6156a6eb7223987487
John <johnzhou721@gmail.com>
2025-07-23 13:40:56 -0500

    more tests!

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from unittest.mock import ANY
 
 import pytest
 
@@ -7,10 +8,11 @@ import qasync
 
 @pytest.fixture
 def get_event_loop_coro(expected_loop):
-    async def coro():
+    async def coro(expected_debug):
         event_loop = asyncio.get_event_loop()
         
         assert type(event_loop).__name__ == expected_loop
+        assert event_loop.get_debug() == expected_debug
         await asyncio.sleep(0)
     return coro
 
@@ -20,13 +22,17 @@ def expected_loop():
 
 def test_run_with_contextmanager(get_event_loop_coro):
     asyncio.set_event_loop(None)
-    qasync.run(get_event_loop_coro())
+    qasync.run(get_event_loop_coro(ANY))
 
     with pytest.raises(RuntimeError):
         _ = asyncio.get_event_loop()
 
-def test_run_reset_policy(get_event_loop_coro, expected_loop):
+def test_run_reset_policy(get_event_loop_coro):
     old_loop = asyncio.new_event_loop()
-    qasync.run(get_event_loop_coro())
+    qasync.run(get_event_loop_coro(ANY))
     new_loop = asyncio.new_event_loop()
     assert type(old_loop) == type(new_loop)
+
+def test_run_debug(get_event_loop_coro):
+    qasync.run(get_event_loop_coro(True), debug=True)
+    qasync.run(get_event_loop_coro(False), debug=False)

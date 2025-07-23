1a1b2ccd8c46e8324ede231e5147223b98095467
Kristján Valur Jónsson <sweskman@gmail.com>
2025-07-23 14:28:21 +0000

    update executor example

--- examples/executor_example.py
+++ examples/executor_example.py
@@ -34,8 +34,4 @@ def last_50(progress, loop):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    event_loop = QEventLoop(app)
-    asyncio.set_event_loop(event_loop)
-
-    event_loop.run_until_complete(master())
-    event_loop.close()
+    asyncio.run(master(), loop_factory=lambda:QEventLoop(app))

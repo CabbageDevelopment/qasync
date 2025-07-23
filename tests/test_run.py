d1b91454e0f23c91a874ef0897f439f932275e19
John <johnzhou721@gmail.com>
2025-07-23 10:38:03 -0500

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,18 +1,23 @@
-import qasync
 import asyncio
 import os
-from qasync import QApplication
+
+import pytest
+
+import qasync
+
 
 def test_run_with_contextmanager(application):
     async def coro():
         event_loop = asyncio.get_event_loop()
-        assert type(event_loop).__name__ == "QIOCPEventLoop" if os.name == 'nt' else "QSelectorEventLoop"
+        assert (
+            type(event_loop).__name__ == "QIOCPEventLoop"
+            if os.name == "nt"
+            else "QSelectorEventLoop"
+        )
         await asyncio.sleep(0)
 
+    asyncio.set_event_loop(None)
     qasync.run(coro())
 
-    try:
-        event_loop = asyncio.get_event_loop()
-    except:
-        event_loop = None
-    assert type(event_loop).__name__ != "QIOCPEventLoop" if os.name == 'nt' else "QSelectorEventLoop"
+    with pytest.raises(RuntimeError):
+        _ = asyncio.get_event_loop()

a544e6e2ce4aee68c37b957274e3310f2cb192c0
John Zhou <john-xyz@outlook.com>
2025-07-22 21:22:32 -0500

    add a simple test to run

---
+++ tests/test_run.py
@@ -0,0 +1,18 @@
+import qasync
+import asyncio
+import os
+from qasync import QApplication
+
+def test_run_with_contextmanager(application):
+    async def coro():
+        event_loop = asyncio.get_event_loop()
+        assert type(event_loop).__name__ == "QIOCPEventLoop" if os.name == 'nt' else "QSelectorEventLoop"
+        await asyncio.sleep(0)
+
+    qasync.run(coro())
+
+    try:
+        event_loop = asyncio.get_event_loop()
+    except:
+        event_loop = None
+    assert type(event_loop).__name__ != "QIOCPEventLoop" if os.name == 'nt' else "QSelectorEventLoop"

5376628d583559bafac43121c1cb500c23758308
Alex March <alexmarch@fastmail.com>
2025-07-24 11:50:43 +0900

    tidy up tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,38 +1,37 @@
 import asyncio
-import os
 from unittest.mock import ANY
 
 import pytest
 
 import qasync
 
+
 @pytest.fixture
-def get_event_loop_coro(expected_loop):
+def get_event_loop_coro():
     async def coro(expected_debug):
         event_loop = asyncio.get_event_loop()
-        
-        assert type(event_loop).__name__ == expected_loop
+        assert type(event_loop) is qasync.QEventLoop
         assert event_loop.get_debug() == expected_debug
         await asyncio.sleep(0)
+
     return coro
 
-@pytest.fixture
-def expected_loop():
-    return "QIOCPEventLoop" if os.name == "nt" else "QSelectorEventLoop"
 
-def test_run_with_contextmanager(get_event_loop_coro):
+def test_qasync_run_restores_loop(get_event_loop_coro):
     asyncio.set_event_loop(None)
     qasync.run(get_event_loop_coro(ANY))
 
     with pytest.raises(RuntimeError):
         _ = asyncio.get_event_loop()
 
-def test_run_reset_policy(get_event_loop_coro):
-    old_loop = asyncio.new_event_loop()
-    qasync.run(get_event_loop_coro(ANY))
-    new_loop = asyncio.new_event_loop()
-    assert type(old_loop) == type(new_loop)
 
-def test_run_debug(get_event_loop_coro):
+def test_qasync_run_restores_policy(get_event_loop_coro):
+    old_policy = asyncio.get_event_loop_policy()
+    qasync.run(get_event_loop_coro(ANY))
+    new_policy = asyncio.get_event_loop_policy()
+    assert type(old_policy) is type(new_policy)
+
+
+def test_qasync_run_with_debug_args(get_event_loop_coro):
     qasync.run(get_event_loop_coro(True), debug=True)
     qasync.run(get_event_loop_coro(False), debug=False)
